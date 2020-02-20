69416c443c1a452ce68750ef86422e3e3426d670
abk <abdelilahkal@gmail.com>
2020-02-20 02:10:59 +0100

    tests description

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,61 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Tic Tac Toe', () => {
+  it('X plays first', () => {
+      
+  })
+
+  it('O plays second', () => {
+      
+  })
+
+  it('disable playing on played position', () => {
+      
+  })
+
+  it('should be able to make move and change turn', () => {
+      
+  })
+
+  it('should win if first row is filled by same player', () => {
+      
+  })
+
+  it('should win if second row is filled by same player', () => {
+      
+  })
+
+  it('should win if third row is filled by same player', () => {
+      
+  })
+
+  it('should win if first column is filled by same player', () => {
+      
+  })
+
+  it('should win if second column is filled by same player', () => {
+      
+  })
+
+  it('should win if third column is filled by same player', () => {
+      
+  })
+
+  it('should win if first diagonal is filled by same player', () => {
+      
+  })
+
+  it('should win if second diagonal is filled by same player', () => {
+      
+  })
+
+  it('end the game with draw if no one', () => {
+      
+  })
+  
+
+
+})
